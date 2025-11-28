61eae392f0d01d6e643beb047bc5481d853f50d2
Gabrielle Novaes <gabriellenovaesgoncalves@gmail.com>
2025-11-28 16:01:36 -0300

    chore: Alteração no ConnectionFactory
    Montagem da estrutura do ConnectionFactory

--- src/factory/ConnectionFactory.java
+++ src/factory/ConnectionFactory.java
@@ -1,5 +1,21 @@
 package factory;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionFactory {
+	private static final String URL = "jdbc:mysql://localhost:3306/planejadospelafe";
+	private static final String USER = "root";
+	private static final String PASSWORD = "fatec";
+	
+	public static Connection getConnection() {
+		try {
+			return DriverManager.getConnection(URL, USER, PASSWORD);
+		}catch (SQLException e) {
+			System.out.println("Erro ao conectar: " + e.getMessage());
+			return null;
+		}
+	}
 
 }
