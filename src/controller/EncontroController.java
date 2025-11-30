597b4087ed2a7d09b4cac96ae932a40c2831fccf
Gabrielle Novaes <gabriellenovaesgoncalves@gmail.com>
2025-11-30 19:28:15 -0300

    chore: Alteração do EncontroController

--- src/controller/EncontroController.java
+++ src/controller/EncontroController.java
@@ -5,5 +5,30 @@ import model.Encontro;
 import java.util.List;
 
 public class EncontroController {
+	
+	private EncontroDAO encontroDAO;
+	
+	public EncontroController() {
+		this.encontroDAO = new EncontroDAO();
+	}
+	
+	public void cadastrar(Encontro encontro) {
+		encontroDAO.cadastrar(encontro);
+	}
+	
+	public void atualizar(Encontro encontro) {
+		encontroDAO.atualizar(encontro);
+	};
 
+	public void excluir(Encontro encontro) {
+		encontroDAO.deletar(encontro);
+	}
+	
+	public List<Encontro> listar(){
+		return encontroDAO.listartodos();
+	}
+	
+	public Encontro buscarId(int idEncontro) {
+		return encontroDAO.buscarId(idEncontro);
+	}
 }
