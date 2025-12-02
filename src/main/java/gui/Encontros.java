4625f4ccd15603f702b5488713439cf8f66ddb1a
Gabrielle Novaes <gabriellenovaesgoncalves@gmail.com>
2025-12-02 19:10:35 -0300

    chore: Atualização da página encontros

--- src/main/java/gui/Encontros.java
+++ src/main/java/gui/Encontros.java
@@ -1,12 +1,13 @@
 package gui;
 
-import javafx.application.Application;
 import javafx.geometry.Pos;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -23,12 +24,22 @@ public class Encontros{
 	public void mostrar(Stage encontroStage) {
 		
 		Button btencontros = new Button ("Encontros");
-		Label barra = new Label("|");
-		Button btcadastromae = new Button ("Cadastrar Mães");
-		Label barra2 = new Label("|");
-		Button btaniversariantes = new Button ("Aniversariantes");
+		btencontros.getStyleClass().add("botaotopo");
 		
-		HBox topo = new HBox(20);
+		Label barra = new Label("|");
+		barra.getStyleClass().add("barra");
+		
+		Button btcadastromae = new Button ("Cadastrar Mães");
+		btcadastromae.getStyleClass().add("botaotopo");
+		
+		Label barra2 = new Label("|");
+		barra2.getStyleClass().add("barra");
+
+		Button btaniversariantes = new Button ("Aniversariantes");
+		btaniversariantes.getStyleClass().add("botaotopo");
+
+		
+		HBox topo = new HBox(40);
 		topo.setAlignment(Pos.CENTER);
 		topo.getChildren().addAll(btencontros, barra, btcadastromae, barra2, btaniversariantes);
 		topo.setId("topo");
@@ -59,7 +70,7 @@ public class Encontros{
 		titulo.setId("titulo");
 		
 		Button btCadastrar = new Button("Cadastrar Encontro");
-		btCadastrar.setId("botaoCadastrar");
+		btCadastrar.getStyleClass().add("botao");
 		
 		btCadastrar.setOnAction(e -> {
 			CadastrarEncontros tela = new CadastrarEncontros();
@@ -86,25 +97,33 @@ public class Encontros{
 			}
 		}	
 		
-		VBox card = new VBox(10); 
+		VBox card = new VBox(20); 
 		card.setId("card");
-		card.setPrefWidth(200); 
+		card.setPrefWidth(800);	
+		card.setPadding(new Insets(10, 0, 10, 0));
 		card.setAlignment(Pos.CENTER);
 		
-		for (Encontro e: encontrosProximos) {
-			Label id =  new Label("ID: " + e.getIdEncontro());
-			Label data = new Label("Data: " + e.getData().toString());
-			Button editar = new Button("Editar");
-			
-			HBox infos = new HBox(10);
-			infos.setAlignment(Pos.CENTER_LEFT);
-			infos.setId("infos");
-			infos.getChildren().addAll(id, data, editar);	
-			card.getChildren().add(infos);
+		if (encontrosProximos.isEmpty()) {
+			Label mensagem = new Label("Não há encontros este mês");
+			mensagem.setId("mensagem");
+			card.getChildren().add(mensagem);
+		}else {
+			for (Encontro e: encontrosProximos) {
+				Label id =  new Label("ID: " + e.getIdEncontro());
+				Label data = new Label("Data: " + e.getData().toString());
+				Button editar = new Button("Editar");
+				
+				HBox infos = new HBox(10);
+				infos.setAlignment(Pos.CENTER_LEFT);
+				infos.setId("infos");
+				infos.getChildren().addAll(id, data, editar);	
+				card.getChildren().add(infos);
+			}
 		}
 		
 		Button btTodosEnc = new Button("Todos Encontros");
-		btTodosEnc.setId("botaotodos");
+		btTodosEnc.getStyleClass().add("botao");
+
 		
 		btTodosEnc.setOnAction(e -> {
 			TodosEncontros tela = new TodosEncontros();
@@ -115,15 +134,23 @@ public class Encontros{
 			}
 		});
 		
-		VBox layout = new VBox(20);
-		layout.setAlignment(Pos.CENTER);
+		VBox layout = new VBox(30);
+		layout.setAlignment(Pos.TOP_CENTER);
+		layout.setPadding(new Insets(50, 0, 0, 0));
 		layout.setId("layout");
-		layout.getChildren().addAll(topo, titulo, btCadastrar, subtitulo, card, btTodosEnc);
+		layout.setFillWidth(false);
+		layout.getChildren().addAll(titulo, btCadastrar, subtitulo, card, btTodosEnc);
 		
-		Scene scene = new Scene(layout);
+		BorderPane root = new BorderPane();
+		root.setTop(topo);
+		root.setCenter(layout);
+		
+		Scene scene = new Scene(root);
 		scene.getStylesheets().add(getClass().getResource("/gui/encontros.css").toExternalForm());
 		
 		encontroStage.setScene(scene);
+		encontroStage.setTitle("Planejados pela Fé");
+		encontroStage.setFullScreen(true);
 	}
 
 
